36a32b587f74fa476a5fc8fe46957a9882fd4baf
solar05 <artem00298@gmail.com>
2021-09-19 01:59:19 +0300

    MVP

--- src/main/scavgen_front/app/core.cljs
+++ src/main/scavgen_front/app/core.cljs
@@ -1,13 +1,54 @@
 (ns scavgen-front.app.core
-  (:require [reagent.dom :as rdom]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.dom :as rdom]
+            [reagent.core :as r]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
+
+(defonce health (r/atom false))
+
+(defn build-url [path]
+  (str "http://scavgen.com/api" path))
+
+(defn healthy? []
+  @health)
+
+(defn check-health []
+  (go (let [response (<! (http/get (build-url "/health")
+                                   {:with-credentials? false}))]
+        (reset! health (:success response)))))
+
+(defn simple-scav []
+  (let [scav-state (r/atom {})]
+    (fn []
+      (let [scav-button [:button
+                         {:on-click (fn []
+                                      (go (let [response (<! (http/get (build-url "/single") {:with-credentials? false}))]
+                                            (reset! health (:success response))
+                                            (when (= (:status response) 200)
+                                              (reset! scav-state (:scavenger (:body response)))))))} "Generate scav!"]]
+      (if (empty? @scav-state)
+        [:div
+         [:p "Try generate new scavenger!"]
+         scav-button]
+        [:div
+         (let [scavenger @scav-state
+               scav-info (str (:fullName scavenger) " " (:rarity scavenger))]
+           [:p scav-info])
+         scav-button])))))
 
 (defn app []
-  [:h1 "Create Reagent App"])
+  [:div
+   (if (healthy?)
+     [simple-scav]
+     [:div [:p "Service temporary unavailable ¯\\_(ツ)_/¯"]
+      [:button {:on-click check-health} "Ping service!"]])])
 
 (defn render []
   (rdom/render [app] (.getElementById js/document "root")))
 
 (defn ^:export main []
+  (check-health)
   (render))
 
 (defn ^:dev/after-load reload! []
